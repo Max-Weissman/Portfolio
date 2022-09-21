5d667ef8c561bb0a2d1079aab7ab3fcba4ff1df9
Max-Weissman <maxweissman@rogers.com>
2022-09-21 18:20:19 +0000

    Removed unneeded npms

--- client/index.js
+++ client/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store/store';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Main from './components/main';
 import './index.css'
 
 ReactDOM.render(
-  <Provider store={store}>
   <Router>
     <Main />
-  </Router>
-  </Provider>,
+  </Router>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
