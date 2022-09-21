8b3f612e6fcbdaff99d9bf3956f6fd85926b8b86
Max-Weissman <maxweissman@rogers.com>
2022-09-21 18:47:32 +0000

    added history

--- client/index.js
+++ client/index.js
@@ -4,8 +4,12 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Main from './components/main';
 import './index.css'
 
+import {createBrowserHistory} from 'history'
+
+const history = createBrowserHistory()
+
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <Main />
   </Router>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
