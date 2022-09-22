ba6f46a7c3f26b643d65261f52a40971c4d295b5
Max-Weissman <maxweissman@rogers.com>
2022-09-22 14:45:38 +0000

    Updates

--- bundle.js
+++ bundle.js
@@ -40915,7 +40915,6 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const history = (0,history__WEBPACK_IMPORTED_MODULE_4__.createBrowserHistory)();
-console.log(history);
 react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_5__.BrowserRouter, {
   history: history
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_main__WEBPACK_IMPORTED_MODULE_2__["default"], {
